94146c60138787407f9526b3b884539fb67b11c0
gadetanaji05 <tgade964@gmail.com>
2026-07-14 23:55:24 +0530

    Sprint 1:ETL Pipeline completed

--- src/loader.py
+++ src/loader.py
@@ -1,3 +1,4 @@
+"""
 import pandas as pd
 
 def main():
@@ -41,6 +42,52 @@ def main():
     print("\nCleaned file saved to output companies_cleaned.xlsx")
     print("\nAfter Cleaning")
     print(df.isnull().sum())
+
+if __name__ == "__main__":
+    main()   """
+
+import os
+import pandas as pd
+
+DATA_FOLDER = "data"
+OUTPUT_FOLDER = "output"
+
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+def process_excel(file_path):
+    print(f"\nProcessing: {os.path.basename(file_path)}")
+
+    df = pd.read_excel(file_path, header=1)
+
+    print("Rows:", len(df))
+    print("Columns:", len(df.columns))
+
+    # Remove duplicate rows
+    df = df.drop_duplicates()
+
+    # Fill missing values
+    df = df.fillna("N/A")
+
+    output_file = os.path.join(
+        OUTPUT_FOLDER,
+        os.path.basename(file_path).replace(".xlsx", "_cleaned.xlsx")
+    )
+
+    df.to_excel(output_file, index=False)
+
+    print("Saved:", output_file)
+
+
+def main():
+
+    for file in os.listdir(DATA_FOLDER):
+
+        if file.endswith(".xlsx"):
+
+            file_path = os.path.join(DATA_FOLDER, file)
+
+            process_excel(file_path)
+
 
 if __name__ == "__main__":
     main()
