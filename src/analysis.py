4ef288f079868020f2cf10af17012a56a7e79f25
gadetanaji05 <tgade964@gmail.com>
2026-07-21 12:33:08 +0530

    Completed Sprint 6-Data Visualizations

--- src/analysis.py
+++ src/analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def load_data():
     df = pd.read_excel("data/analysis.xlsx", header=1)
@@ -26,6 +27,71 @@ def summery_statistics(df):
     print("\nsummary Statistics:")
     print(df.describe(include="all"))
 
+def sales_growth_chart(df):
+    plt.figure(figsize=(10,5))
+    df["growth"] = (
+    df["compounded_sales_growth"].str.extract
+    (r"(\d+)%")[0].astype(float))
+    plt.bar(df["company_id"],df["growth"])
+    plt.title("Compounded Sales Growth")
+    plt.xlabel("Company")
+    plt.ylabel("Sales Growth")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig("output/sales_growth_chart.png")
+    plt.show()
+
+
+
+def roe_chart(df):
+    plt.figure(figsize=(10,5))
+
+    df["roe_value"]=(
+        df["roe"]
+        .str.extract(r"(\d+)%")[0]
+        .astype(float)
+    )
+    print(df["roe"].head(20))
+    print(df["roe_value"])
+    
+    plt.bar(df["company_id"],
+            df["roe_value"])
+    plt.title("return on Equality (ROE)")
+    plt.xlabel("Company")
+    plt.ylabel("ROE(%)")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig("output/roe_chart.png")
+
+    plt.show()
+
+def stock_cagr_chart(df):
+    plt.figure(figsize=(10,5))
+
+    df["stock_cagr_value"]=(
+        df["stock_price_cagr"]
+        .str.extract(r"(\d+)%")[0]
+        .astype(float)
+    )
+    plt.bar(df["company_id"],
+            df["stock_cagr_value"])
+    plt.title("Stock Price CAGR")
+    plt.xlabel("Company")
+    plt.ylabel("Stock CAGR (%)")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig("output/stock_cagr_chart.png")
+    plt.show()
+
+def company_distribution(df):
+    company_counts = df["company_id"].value_counts()
+    plt.figure(figsize=(7,7))
+    plt.pie(company_counts,labels=company_counts.index,autopct="%1.1f%%",startangle=90)
+    plt.title("company Distribution")
+    plt.axis("equal")
+    plt.savefig("output/company_distribution_chart.png")
+    plt.show()
+
 def main():
     df = load_data()
     total_records(df)
@@ -35,6 +101,10 @@ def main():
     summery_statistics(df)
     print("\nFirst 5 Records")
     print(df.head())
+    sales_growth_chart(df)
+    roe_chart(df)
+    stock_cagr_chart(df)
+    company_distribution(df)
 
 if __name__=="__main__":
     main()
